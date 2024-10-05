6e80c4612979382ef1c7f733cb3a5a9719c4e7e4
Bruno <brunosca96@gmail.com>
2024-10-05 11:32:51 -0700

    Documentsupdate

---
+++ public/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYt743OXIJxq_4UjjfpJeXgg8S_McOd_Y",
+  authDomain: "portfoliov1brunoscaramutti.firebaseapp.com",
+  projectId: "portfoliov1brunoscaramutti",
+  storageBucket: "portfoliov1brunoscaramutti.appspot.com",
+  messagingSenderId: "454962774860",
+  appId: "1:454962774860:web:bf9524c1cd856acdc145e9"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
